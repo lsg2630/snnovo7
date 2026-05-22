b3fda463ff892eb777b3e9107b210cc1b34a425f
Letícia Soares <leticiasoares2122@gmail.com>
2026-05-21 22:07:48 -0300

    Delete primeirositedenoticias-main directory

--- primeirositedenoticias-main/assets/js/menu-hamburguer.js
+++
@@ -1,4 +0,0 @@
-function alternarVizualizacaoDoMenuLateral(){
-    const menu = document.querySelector("nav");
-    menu.classList.toggle("menu-oculto"); // toggle: chavear(se não tem, coloque. se tem, retire! (alternar entre ver e não ver))
-}
